e85876ab2b213cf85353f7d0cf6a53f3736255d6
jay3ss <mentoce@gmail.com>
2024-09-02 15:39:41 -0700

    Update types

--- src/@types/deals.d.ts
+++ src/@types/deals.d.ts
@@ -1,10 +1,36 @@
 import { IMetrics } from "./metrics";
 
-export interface IDeal {}
+export interface IDeal {
+    id: number | string;
+    name: string;
+    sqft: number;
+    term: number;
+    baseRent: number;
+    annualEscalations: number;
+    opExPerMonthRsf: number;
+    globalInflation: number;
+    otherOneTimeLandlordCost: number;
+    otherOneTimeTenantCost: number;
+    otherMonthlyLandlordCost: number;
+    otherMonthlyTenantCost: number;
+    monthsFreeRent: number;
+    commissionFirst: number;
+    commissionSecond: number;
+    tenantDiscountRate: number;
+    tiCostPerRsf: number;
+    tiAllowancePerRsf: number;
+    landlordDiscountRate: number;
+}
 
-export interface DealsContextType {
+export type DealsContextType = {
     deals: IDeal[];
     metrics: IMetrics[];
     dealsDispatch: (params: Record<string, number | string>) => any;
-    metricsDispatch?: (params: Record<string, number | string>) => any;
+    metricsDispatch: (params: Record<string, number | string>) => any;
+};
+
+export interface DealInputsFormProps {
+    deal: IDeal;
+    disabled: boolean;
+    handleChange: (e: any, key: string | number, type?: string) => void;
 }
