2abcb159405feb342301bd6501b7f1838ac39ce7
jay3ss <mentoce@gmail.com>
2024-09-02 16:58:59 -0700

    Add dollar and percent sign adornments

---
+++ src/components/Adornments/Adornments.tsx
@@ -0,0 +1,8 @@
+import { InputAdornment } from "@mui/material";
+
+export const dollarAdornment = (
+    <InputAdornment position="start">$</InputAdornment>
+);
+export const percentAdornment = (
+    <InputAdornment position="end">%</InputAdornment>
+);
