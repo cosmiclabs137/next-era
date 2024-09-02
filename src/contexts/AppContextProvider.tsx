8851dea52354cc9e9e9930da0ab795051937e2a7
jay3ss <mentoce@gmail.com>
2024-09-02 15:45:29 -0700

    Use proper (hopefully) types and add default data

--- src/contexts/AppContextProvider.tsx
+++ src/contexts/AppContextProvider.tsx
@@ -3,15 +3,29 @@ import React from "react";
 import { DealsContextType, IDeal } from "@/@types/deals";
 import { IMetrics } from "@/@types/metrics";
 
-interface AppContextProviderProps {
+import { dealFactory } from "@/utils";
+
+export interface AppContextProviderProps {
     children: React.ReactElement<any, any> | string | null;
 }
 
-export const DealsContext = React.createContext<DealsContextType | null>(null);
+const defaultContext: DealsContextType = {
+    deals: [dealFactory(0), dealFactory(1), dealFactory(2)],
+    metrics: [{}],
+    dealsDispatch: (params: Record<string, number | string>) =>
+        console.log("dealsDispatch() called"),
+    metricsDispatch: (params: Record<string, number | string>) =>
+        console.log("metricsDispatch() called"),
+};
+
+export const DealsContext =
+    React.createContext<DealsContextType>(defaultContext);
 
 const AppContextProvider = ({ children }: AppContextProviderProps) => {
-    const [deals] = React.useState<IDeal[]>([]);
-    const [metrics] = React.useState<IMetrics[]>([]);
+    const [deals, setDeals] = React.useState<IDeal[]>(defaultContext.deals);
+    const [metrics, setMetrics] = React.useState<IMetrics[]>(
+        defaultContext.metrics
+    );
 
     const dealsDispatch = () => console.log("dealsDispatch() called");
     const metricsDispatch = () => console.log("metricsDispatch() called");
