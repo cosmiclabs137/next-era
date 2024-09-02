370cb3101eb322607b42afbd447c19f50b409d51
jay3ss <mentoce@gmail.com>
2024-09-02 15:52:19 -0700

    Add collapsible component

---
+++ src/components/Collapsible/Collapsible.tsx
@@ -0,0 +1,31 @@
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+interface CollapsibleProps {
+    children: React.ReactElement<any, any> | string | null;
+    defaultExpanded: boolean;
+    id: string;
+    summary: string;
+}
+
+const Collapsible = ({
+    id,
+    summary,
+    children,
+    defaultExpanded = false,
+}: CollapsibleProps) => {
+    return (
+        <Accordion defaultExpanded={defaultExpanded}>
+            <AccordionSummary
+                id={id}
+                aria-label={id}
+                expandIcon={<ExpandMoreIcon />}
+            >
+                {summary}
+            </AccordionSummary>
+            <AccordionDetails>{children}</AccordionDetails>
+        </Accordion>
+    );
+};
+
+export default Collapsible;
