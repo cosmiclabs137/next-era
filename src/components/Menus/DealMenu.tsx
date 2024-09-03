b7b71242d03606bc8cfcb083992f2fd3dbd6338d
jay3ss <mentoce@gmail.com>
2024-09-02 20:36:45 -0700

    Implement deal menu (looks like it's working as it was in last version)

--- src/components/Menus/DealMenu.tsx
+++ src/components/Menus/DealMenu.tsx
@@ -1,5 +1,28 @@
 import React, { Dispatch, SetStateAction } from "react";
 
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    IconButton,
+    Tooltip,
+} from "@mui/material";
+import DeleteSharpIcon from "@mui/icons-material/DeleteSharp";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import LockIcon from "@mui/icons-material/Lock";
+import SaveIcon from "@mui/icons-material/Save";
+
+import {
+    usePopupState,
+    bindTrigger,
+    bindDialog,
+} from "material-ui-popup-state/hooks";
+
 interface DealMenuProps {
     dealId: number;
     disabled: boolean;
@@ -7,8 +30,72 @@ interface DealMenuProps {
 }
 
 const DealMenu = ({ dealId, disabled, setDisabled }: DealMenuProps) => {
-    console.log(dealId, disabled, setDisabled);
-    return <div>DealMenu</div>;
+    const popupState = usePopupState({
+        variant: "dialog",
+    });
+    return (
+        <div style={{ width: "100%" }}>
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    textAlign: "center",
+                    justifyContent: "center",
+                    justifyItems: "center",
+                    m: 1,
+                }}
+            >
+                <Tooltip title={disabled ? "Enable input" : "Disable input"}>
+                    <IconButton onClick={() => setDisabled(!disabled)}>
+                        {disabled ? <LockIcon /> : <LockOpenOutlinedIcon />}
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Delete deal">
+                    <IconButton {...bindTrigger(popupState)}>
+                        <DeleteSharpIcon />
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Clone deal">
+                    <IconButton>
+                        <FileCopyIcon />
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Save deal">
+                    <IconButton>
+                        <SaveIcon />
+                    </IconButton>
+                </Tooltip>
+            </Box>
+            <Dialog
+                {...bindDialog(popupState)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Delete this deal?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        This will delete this deal. There is no way to recover
+                        it after you confirm deletion. Proceed?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={popupState.close}>Cancel</Button>
+                    <Button
+                        onClick={popupState.close}
+                        autoFocus
+                        sx={{ color: "red" }}
+                    >
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 };
 
 export default DealMenu;
