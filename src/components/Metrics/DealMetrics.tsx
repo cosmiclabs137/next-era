d15e0faae330cd894296a3b3fe90799d40453868
jay3ss <mentoce@gmail.com>
2024-09-02 20:31:48 -0700

    Implement deal metrics component (not exactly working, gotta adapt it to this app)

--- src/components/Metrics/DealMetrics.tsx
+++ src/components/Metrics/DealMetrics.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 
+import Stack from "@mui/material/Stack";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+
+import { toCurrency } from "../../finance";
+
+import { styled } from "@mui/material/styles";
+import { Container } from "@mui/material";
+
+const MetricsText = ({ children }: any) => (
+    <Typography variant="subtitle1" color="InfoText" gutterBottom>
+        {children}
+    </Typography>
+);
+
+const Item = styled(Container)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    fontSize: "0.7rem",
+}));
+
 import { IMetrics } from "@/types/metrics";
 
 interface DealMetricsProps {
@@ -7,8 +31,42 @@ interface DealMetricsProps {
 }
 
 const DealMetrics = ({ metrics }: DealMetricsProps) => {
-    console.log(metrics);
-    return <div>DealMetrics</div>;
+    const landlord = metrics?.landlord;
+    const tenant = metrics?.tenant;
+
+    return (
+        <Stack direction="row" spacing={2} justifyContent="space-around">
+            <Item>
+                <Tooltip
+                    arrow
+                    followCursor
+                    title="Landlord net effective rate / RSF."
+                    placement="top"
+                    describeChild
+                >
+                    <span>
+                        <MetricsText>
+                            Landlord: {toCurrency(landlord)}
+                        </MetricsText>
+                    </span>
+                </Tooltip>
+            </Item>
+
+            <Item>
+                <Tooltip
+                    arrow
+                    followCursor
+                    title="Tenant net effective rate / RSF."
+                    placement="top"
+                    describeChild
+                >
+                    <span>
+                        <MetricsText>Tenant: {toCurrency(tenant)}</MetricsText>
+                    </span>
+                </Tooltip>
+            </Item>
+        </Stack>
+    );
 };
 
 export default DealMetrics;
